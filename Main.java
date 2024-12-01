3384b0e0e9cbaba25738a74a22a6107dbb0ebc4a
defoogay <sstteeaammuser101@gmail.com>
2024-11-30 22:38:47 -0500

    commit

--- Main.java
+++ Main.java
@@ -1,31 +1,18 @@
-import java.util.ArrayList;
+
 
 import Classes.Circle;
-import Classes.Grid;
 import Classes.Point;
-import Classes.RightTriangle;
 import Classes.Shape;
 import Classes.Square;
-import Inheritence.LibraryItem;
 
 public class Main {
     public static void main(String[] args) {
-        Point yes = new Point(15,15);
-        Point maybe = new Point(10, 10);
-        Square no = new Square(maybe, 10);
-        System.out.println(no.isOn(yes));
-        Point[] Corners = no.corners();
-        for (int i = 0; i < 4; i++){
-            System.out.println(Corners[i]);
-        }
         Shape[] shapes = {new Circle(new Point(1.8, -20), 2), 
             new Square(new Point(100, 2.1), 5.4),
             new Circle(new Point(0, 0), 1),
             new Circle(new Point(4, 9.123), 98.32),
             new Square(new Point(-321, 0), 0.02)};
             System.out.println(sumArea(shapes));
-        RightTriangle triFour = new RightTriangle(maybe, 10.0, 10.0);
-        System.out.println(triFour.isOn(yes));
     }
     static double sumArea(Shape[] shapes) {
         double sum = 0;
--- Testing/TestRightTriangle.java
+++ Testing/TestRightTriangle.java
@@ -4,14 +4,14 @@ import Classes.Point;
 import Classes.RightTriangle;
 
 public class TestRightTriangle {
-    public static void maiin(String[] args) {
-        Point yes = new Point(15,15);
+    public static void main(String[] args) {
+        Point yes = new Point(13,13);
         Point maybe = new Point(10, 10);
         RightTriangle triFour = new RightTriangle(maybe, 10.0, 10.0);
 
         System.out.println(triFour.isOn(yes));
 
-        System.out.println(triFour.isInside(maybe));
+        System.out.println(triFour.isInside(yes));
 
         RightTriangle newTri = new RightTriangle(maybe, 20.0, 20.0);
         System.out.println(RightTriangle.similar(triFour, newTri));
